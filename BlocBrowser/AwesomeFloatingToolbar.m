fb0e3e8b855b06de4b04dd0a542a6483a0c89bea
Elyse Erb <jerb@me.com>
2016-08-02 13:40:25 -0700

    Working on checkpoint

---
+++ BlocBrowser/AwesomeFloatingToolbar.m
@@ -0,0 +1,18 @@
+//
+//  UIView+AwesomeFloatingToolbar.m
+//  BlocBrowser
+//
+//  Created by Elyse Erb on 8/1/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "AwesomeFloatingToolbar.h"
+
+@interface AwesomeFloatingToolbar ()
+
+@property (nonatomic, strong) NSArray *currentTitles;
+@property (nonatomic, strong) NSArray *colors;
+@property (nonatomic, strong) NSArray *labels;
+@property (nonatomic, weak) UILabel *currentLabel;
+
+@end
